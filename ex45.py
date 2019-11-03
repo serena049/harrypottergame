a3e6c71ad5975a0fcdd716c602b241382f08f0fb
Zou Serena <zou.serena@bcg.com>
2019-11-03 09:06:31 -0700

    fill in class info, start creating the game...

--- ex45.py
+++ ex45.py
@@ -35,24 +35,42 @@ Engine
 - play
 """
 
+from sys import exit
+from random import randint
 
 class Scene(object):
 
     def enter(self):
-        pass
+        print "entering base scene..."
+        exit(1)
 
 class Engine(object):
 
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene_map
 
     def play(self):
-        pass
+        current_scene = self.scene_map.go_to_the_first_scene()
+        final_scene = self.scene_map.go_to_scene('finished')
+
+        while current_scene != final_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.go_to_scene(next_scene_name)
+
+        # print out the final scene
+        current_scene.enter()
 
 class Death(Scene):
 
+    quips = [
+    "You died. You are definitely not a Harry Potter Fan!",
+    "Shame on you! You failed to save your future wife!",
+    "I bet Dobby can do a better job than you!",
+    "This is not the right game for you, go back to Dota2!"
+    ]
+
     def enter(self):
-        pass
+        print De
 
 class GreatHall(Scene):
 
@@ -74,16 +92,35 @@ class GryfiindorsCommonRoom(Scene):
     def enter(self):
         pass
 
+class Finished(Scene):
+
+    def enter(self):
+        pass
+
+    def next_scene(self):
+        pass
+
 class Map(object):
 
+    scenes = {
+        'death': Death(),
+        'greathall': GreatHall(),
+        'astronomytower': AstronomyTower(),
+        'darkforest': DarkForest(),
+        'gryfiindorscommonroom': GryfiindorsCommonRoom(),
+        'finished': Finished()
+
+    }
+
     def __init__(self, start_scene):
-        pass
+        self.start_scene = start_scene
 
-    def next_scene(self, scene_name):
-        pass
+    def go_to_scene(self, scene_name):
+        scene_class = Map.scenes.get(scene_name)
+        return scene_class
 
-    def opening_scene(self):
-        pass
+    def go_to_the_first_scene(self):
+        return self.go_to_the_first_scene(self.start_scene)
 
 
 a_map = Map('Hogwarts')
